9d2f942cc8fe4decd02e3e64c111c3402e19c6ba
Penina Cheshin <penk85@gmail.com>
2020-11-09 10:36:29 +0100

    changes

--- packages/editor/__tests__/specs/articleMetadataPanel.tsx
+++ packages/editor/__tests__/specs/articleMetadataPanel.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {MockedProvider as MockedProviderBase} from '@apollo/client/testing'
-import {AuthorEditPanel} from '../../src/client/panel/authorEditPanel'
-import {CreateAuthorDocument, AuthorDocument} from '../../src/client/api'
+import {ArticleMetadata, ArticleMetadataPanel} from '../../src/client/panel/articleMetadataPanel'
+//import {  } from '../../src/client/api'
 import {mount} from 'enzyme'
 
 import {UIProvider} from '@karma.run/ui'
@@ -21,3 +21,19 @@ const styleRenderer: fela.IRenderer = {
   subscribe: jest.fn(),
   clear: jest.fn()
 }
+/*
+describe('Article Metadata Panel', async () => {
+  test('should render', () => {
+    const wrapper = mount(
+      <UIProvider styleRenderer={styleRenderer} rootElementID={'fskr'}>
+        <MockedProvider  addTypename={false}>
+          <ArticleMetadataPanel />
+        </MockedProvider>
+      </UIProvider>
+    )
+    await updateWrapper(wrapper, 100)
+
+    const panel = wrapper.find('PeerEditPanel')
+    expect(panel).toMatchSnapshot()
+  }
+*/
