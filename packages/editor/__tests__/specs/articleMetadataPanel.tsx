c2ab360a5c56f5504bc6e213587cbee88553c42e
Penina Cheshin <penk85@gmail.com>
2020-11-12 11:16:13 +0100

    author metadata snapshot

--- packages/editor/__tests__/specs/articleMetadataPanel.tsx
+++ packages/editor/__tests__/specs/articleMetadataPanel.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {MockedProvider as MockedProviderBase} from '@apollo/client/testing'
-import {ArticleMetadata, ArticleMetadataPanel} from '../../src/client/panel/articleMetadataPanel'
-//import {  } from '../../src/client/api'
+import {ArticleMetadataPanel} from '../../src/client/panel/articleMetadataPanel'
+import {AuthorListDocument} from '../../src/client/api'
 import {mount} from 'enzyme'
 
 import {UIProvider} from '@karma.run/ui'
@@ -21,19 +21,77 @@ const styleRenderer: fela.IRenderer = {
   subscribe: jest.fn(),
   clear: jest.fn()
 }
-/*
-describe('Article Metadata Panel', async () => {
-  test('should render', () => {
+const article = {
+  slug: 'the-properties-of-moonstone-and-its-uses',
+  preTitle: 'Moonstone Uses',
+  title: 'The Properties of Moonstone And Its Uses',
+  lead: '',
+  authors: [
+    {
+      id: 'authorId2',
+      name: 'Hermione Granger'
+    }
+  ],
+  tags: ['moonstone', 'potions'],
+  properties: [
+    {
+      key: 'abc',
+      value: 'def',
+      public: false
+    }
+  ],
+  image: undefined,
+  shared: false,
+  breaking: true
+}
+
+const mocks = [
+  {
+    request: {
+      query: AuthorListDocument,
+      variables: {
+        first: 10,
+        filter: undefined
+      }
+    },
+    result: () => ({
+      data: {
+        authors: {
+          nodes: [
+            {
+              __typename: 'Author',
+              id: 'authorId2',
+              name: 'Hermione Granger',
+              slug: 'hermione-granger',
+              //url: 'www.hg.ch/',
+              links: [{title: 'Link Title', url: 'www.link-url.ch/'}],
+              //bio: createDefaultValue(),
+              image: undefined
+            }
+          ],
+          pageInfo: {
+            hasNextPage: false,
+            hasPreviousPage: false
+          },
+          totalCount: 1
+        }
+      }
+    })
+  }
+]
+
+describe('Article Metadata Panel', () => {
+  test('should render', async () => {
     const wrapper = mount(
       <UIProvider styleRenderer={styleRenderer} rootElementID={'fskr'}>
-        <MockedProvider  addTypename={false}>
-          <ArticleMetadataPanel />
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <ArticleMetadataPanel value={article} />
         </MockedProvider>
       </UIProvider>
     )
     await updateWrapper(wrapper, 100)
 
-    const panel = wrapper.find('PeerEditPanel')
+    const panel = wrapper.find('ArticleMetadataPanel')
     expect(panel).toMatchSnapshot()
-  }
-*/
+  })
+})
